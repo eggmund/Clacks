794da61e3df0a6718bae96ed9f243fd7e18a082d
eggmund <joshuacolclough2@googlemail.com>
2019-07-15 14:58:07 +0100

    Added floor

--- src/block.rs
+++ src/block.rs
@@ -6,15 +6,17 @@ use crate::FLOOR_POS;
 #[derive(Default)]
 pub struct Block {
     pub pos: f64,       // One dimensional
+    min_pos: f64,
     pub vel: f64,
     pub mass: f64,
-    pub size: f32,
+    pub size: f64,
 }
 
 impl Block {
-    pub fn new(pos: f64, vel: f64, mass: f64, size: f32) -> Block {
+    pub fn new(pos: f64, vel: f64, mass: f64, size: f64, min_pos: f64) -> Block {
         Block {
             pos,
+            min_pos,
             vel,
             mass,
             size
@@ -25,20 +27,23 @@ impl Block {
         let sqr = Mesh::new_rectangle(
             ctx,
             DrawMode::fill(),
-            Rect::new(0.0, 0.0, self.size, self.size),
+            Rect::new(0.0, 0.0, self.size as f32, self.size as f32),
             [1.0, 0.0, 0.1, 1.0].into()
         )?;
         
         graphics::draw(ctx, &sqr,
             DrawParam::default()
-                .dest([self.pos as f32, FLOOR_POS - self.size])
-        )?;
-        Ok(())
+                .dest([
+                    self.pos as f32,
+                    FLOOR_POS - self.size as f32
+                ])
+        )
     }
 
     #[inline]
     pub fn update_pos(&mut self, dt: f64) {
         self.pos += self.vel * dt;
+        self.pos = self.pos.max(self.min_pos);
     }
 
     pub fn bounce(&mut self, other: &Self) -> f64 {
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 mod block;
 
 use ggez::event;
-use ggez::graphics;
-use ggez::timer;
+use ggez::graphics::{self, DrawParam, Mesh, DrawMode, Rect};
+//use ggez::timer;
 use ggez::{Context, GameResult};
 
 use crate::block::Block;
 
 pub const FLOOR_POS: f32 = 500.0;
-pub const ITERATIONS: usize = 100000;
+pub const ITERATIONS: usize = 1000;
 const DELTA_TIME: f64 = 1.0/144.0;
 const DELTA_PER_ITER: f64 = DELTA_TIME/ITERATIONS as f64;
+const SMALL_BLOCK_SIZE: f64 = 20.0;
+const LARGE_BLOCK_SIZE: f64 = 100.0;
 
-
-#[derive(Default)]
 struct MainState {
     small_block: Block,
     large_block: Block,
@@ -23,21 +23,34 @@ struct MainState {
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
         let s = MainState {
-            small_block: Block::new(100.0, 0.0, 1.0, 20.0),
-            large_block: Block::new(600.0, -100.0, (100.0_f64).powi(3), 100.0),
-            ..Default::default()
+            small_block: Block::new(10.0, 0.0, 1.0, SMALL_BLOCK_SIZE, 0.0),
+            large_block: Block::new(40.0, -2.0, (100.0_f64).powi(60), LARGE_BLOCK_SIZE, SMALL_BLOCK_SIZE),
+            collision_count: 0,
         };
         Ok(s)
     }
 
+    #[inline]
     fn was_collision(&mut self) {
         self.collision_count += 1;
-        println!("Collisions: {}", self.collision_count);
+    }
+
+    fn draw_collision_count(&self, ctx: &mut Context) -> GameResult {
+        use graphics::Text;
+        let text = Text::new(format!("Collisions: {}", self.collision_count));
+
+        graphics::draw(
+            ctx,
+            &text,
+            DrawParam::default()
+                .dest([10.0, 10.0])
+                .color([0.0, 0.0, 0.0, 1.0].into())
+        )
     }
 }
 
 impl event::EventHandler for MainState {
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
         //let dt = timer::duration_to_f64(timer::delta(ctx))/ITERATIONS as f64;
 
         for _ in 0..ITERATIONS {
@@ -52,7 +65,7 @@ impl event::EventHandler for MainState {
             }
 
             if self.small_block.hit_wall(0.0) { self.was_collision(); }
-            self.large_block.hit_wall(0.0);
+            //self.large_block.hit_wall(0.0);
 
             self.small_block.update_pos(DELTA_PER_ITER);
             self.large_block.update_pos(DELTA_PER_ITER);
@@ -66,21 +79,45 @@ impl event::EventHandler for MainState {
         self.small_block.draw(ctx)?;
         self.large_block.draw(ctx)?;
 
+        // Draw floor
+        let floor = Mesh::new_rectangle(
+            ctx,
+            DrawMode::fill(),
+            Rect::new(0.0, 0.0, 10000.0, 1000.0),
+            [0.0, 0.0, 0.0, 1.0].into()
+        )?;
+
+        graphics::draw(ctx, &floor, DrawParam::default().dest([0.0, FLOOR_POS]))?;
+
+        self.draw_collision_count(ctx)?;
         graphics::present(ctx)?;
         Ok(())
     }
 }
 
 fn main() -> GameResult {
-    use ggez::conf::{WindowSetup, NumSamples};
+    use ggez::conf::{WindowSetup, WindowMode, NumSamples};
 
-    let cb = ggez::ContextBuilder::new("Clacks", "eggmund")
+    let mut cb = ggez::ContextBuilder::new("Clacks", "eggmund")
         .window_setup(WindowSetup {
             title: "Clacks".to_owned(),
             samples: NumSamples::Two,
             vsync: true,
             ..Default::default()
+        })
+        .window_mode(WindowMode {
+            width: 900.0,
+            height: 600.0,
+            ..Default::default()
         });
+
+    if let Ok(manifest_dir) = std::env::var("CARGO_MANIFEST_DIR") {
+        let mut path = std::path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        println!("Adding path {:?}", path);
+        cb = cb.add_resource_path(path);
+    }
+
     let (ctx, event_loop) = &mut cb.build()?;
     let state = &mut MainState::new(ctx)?;
     event::run(ctx, event_loop, state)
